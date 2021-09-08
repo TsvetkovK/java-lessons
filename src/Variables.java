d122f7f3c929b69f7a6304a9d35119d7b1e7db4b
Tsvetkov Kirill <tsvetkovk@icloud.com>
2021-09-08 13:36:01 +0400

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 1;
+        short myShort = 3;
+        long myLong = 12123;
+
+        double myDouble = 235.3;
+        float myFloat = 2324.4f;
+
+        char c = 'C';
+        boolean b = true;
+
+        byte myByte = 100; //-128 - 128
+        System.out.println(myInt);
+    }
+}
