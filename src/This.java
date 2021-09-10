92a5204437a81ae434c4519ddc4f602645be4472
Tsvetkov Kirill <tsvetkovk@icloud.com>
2021-09-10 13:39:01 +0400

    This2

--- src/This.java
+++ src/This.java
@@ -1,15 +1,29 @@
 public class This {
     public static void main(String[] args) {
         Human human1 = new Human();
-        human1.setAge(18);
-        human1.setName("Tom");
-        human1.getInfo();
     }
 }
 
 class Human{
     private String name;
     private int age;
+
+
+
+    public Human(String name){
+        System.out.println("Привет из первого конструктора");
+        this.name = name;
+    }
+
+    public Human(){
+        System.out.println("Привет из второго конструктора");
+    }
+
+    public Human(String name, int age){
+        System.out.println("Привет из 3 конструктора");
+        this.name = name;
+        this.age = age;
+    }
 
     public void setName(String name){
         this.name = name;
