11b988e377841eb2674215dbbd639c96f0e8153d
Tsvetkov Kirill <tsvetkovk@icloud.com>
2021-09-13 00:03:14 +0400

    Interfaces

---
+++ src/Interfaces/Animal.java
@@ -0,0 +1,18 @@
+package Interfaces;
+
+public class Animal implements Info{
+    public int id;
+
+    public Animal(int id){
+        this.id = id;
+    }
+
+    public void sleep(){
+        System.out.println("Im sleeping");
+    }
+
+    public void showInfo(){
+        System.out.println(id);
+    }
+
+}
