572d1349d2d667def195fc968c87feb7a5437f2c
Tsvetkov Kirill <tsvetkovk@icloud.com>
2021-09-13 12:03:50 +0400

    Lesson28Access

---
+++ src/Lesson28AccessModifiers/Test.java
@@ -0,0 +1,14 @@
+package Lesson28AccessModifiers;
+
+public class Test {
+    private int id;
+
+    public static void main(String[] args) {
+        //public private default(пакетный) protected
+    }
+
+    private void techMethod(){
+
+    }
+
+}
